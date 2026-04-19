2f922c6c22e63800bd7797fb2d00892d40e501db
khalilaazizagunawan <jioxyjj@gmail.com>
2026-04-19 18:24:18 +0700

    fix: ignore public directory in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,14 @@ export default [
     js.configs.recommended,
     ...pluginVue.configs['flat/recommended'],
     {
+        ignores: ['public/**'],
+    },
+    {
         files: ['**/*.{js,vue}'],
         rules: {
             'vue/multi-word-component-names': 'off',
             'vue/html-indent': ['error', 4],
             'no-undef': 'off', // So things like 'process' or 'route' (Ziggy) don't error
-        }
-    }
+        },
+    },
 ];
