f1788a1c999eb89377e69f07f3f81e02e924164f
reubenjanuardi <reubenjanuardi@gmail.com>
2026-04-19 20:33:05 +0700

    SBA-44 - Fix error page input mahasiswa

--- app/Filament/Resources/Mahasiswas/Schemas/MahasiswaInfolist.php
+++ app/Filament/Resources/Mahasiswas/Schemas/MahasiswaInfolist.php
@@ -5,8 +5,8 @@ namespace App\Filament\Resources\Mahasiswas\Schemas;
 use App\Models\Mahasiswa;
 use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\ImageEntry;
-use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class MahasiswaInfolist
