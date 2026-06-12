4602deda6ed4d5acb2e30ac9c24ed47f4eea7ea0
khalilaazizagunawan <jioxyjj@gmail.com>
2026-06-12 20:52:38 +0700

    SBA-76 PBI#25 - Riwayat Versi Draft Skripsi

--- routes/web/mahasiswa.php
+++ routes/web/mahasiswa.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Mahasiswa\ProgressReminderController;
 use App\Http\Controllers\Mahasiswa\StudyPlanController;
 use App\Http\Controllers\MonitoringJadwalBimbinganController;
 use App\Http\Controllers\DraftSkripsiController;
+use App\Http\Controllers\ExportBimbinganController;
 use Illuminate\Support\Facades\Route;
 
 /*
